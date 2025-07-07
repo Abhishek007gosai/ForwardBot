4c80669e2c8504d71b4593d03b59c5cc472b0450
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2025-07-08 03:06:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "29245477")
     API_HASH = environ.get("API_HASH", "0abc83883262245c90ca337b7a0375c4")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7404600334:AAGpQv2DJb-s7yjXsgmH9_0pBOVFw7O1HXE") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
     DATABASE_URI = "mongodb+srv://Kafka:Au3OoWzCDYJKeuHU@cluster0.lz2m8iy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     DATABASE_NAME = environ.get("DATABASE_NAME", "cluster0")
